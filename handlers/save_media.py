802301e91cd90db95d07441b15a65003f15c6dca
Sk8903 <85931125+Sk8903@users.noreply.github.com>
2021-08-31 13:05:36 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -53,8 +53,8 @@ async def SaveBatchMediaInChannel(bot: Client, editable: Message, message_ids: l
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/DevsZone")]]
+                 [InlineKeyboardButton("Bots Channel", url="https://t.me/Sk_Tv_Official"),
+                  InlineKeyboardButton("Support Group", url="https://t.me/Sk_Tv_Group")]]
             ),
             disable_web_page_preview=True
         )
@@ -93,8 +93,8 @@ async def SaveMediaInChannel(bot: Client, editable: Message, message: Message):
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/DevsZone")]]
+                 [InlineKeyboardButton("Bots Channel", url="https://t.me/Sk_Tv_Official"),
+                  InlineKeyboardButton("Support Group", url="https://t.me/Sk_Tv_Group")]]
             ),
             disable_web_page_preview=True
         )
