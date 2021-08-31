f408d79f629b2f97dfd9ee6a3815380c683c21e8
Sk8903 <85931125+Sk8903@users.noreply.github.com>
2021-08-31 12:55:00 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -11,10 +11,10 @@ from handlers.helpers import str_to_b64
 async def ReplyForward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"**Here is Sharable Link of this file:**\n"
-            f"https://t.me/{Config.BOT_USERNAME}?start=AbirHasan2005_{str_to_b64(str(file_id))}\n\n"
+            f"💖 Join And Support Us 💖"
+            f""
             f"__To Retrive the Stored File, just open the link!__",
-            disable_web_page_preview=True, quote=True)
+            disable_web_page_preview=True, quote=False)
     except FloodWait as e:
         await asyncio.sleep(e.x)
         await ReplyForward(message, file_id)
