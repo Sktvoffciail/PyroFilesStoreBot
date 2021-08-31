649ed88835057f4067f97d954f22d829361e61a9
Sk8903 <85931125+Sk8903@users.noreply.github.com>
2021-08-31 17:36:30 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -1,4 +1,4 @@
-# (c) @AbirHasan2005
+# (c) @JAsuran
 
 import asyncio
 from configs import Config
@@ -12,7 +12,7 @@ async def ReplyForward(message: Message, file_id: int):
     try:
         await message.reply_text(
             f"**Here is Sharable Link of this file:**\n"
-            f"https://t.me/{Config.BOT_USERNAME}?start=AbirHasan2005_{str_to_b64(str(file_id))}\n\n"
+            f"https://t.me/{Config.BOT_USERNAME}?start=JAsuran_{str_to_b64(str(file_id))}\n\n"
             f"__To Retrive the Stored File, just open the link!__",
             disable_web_page_preview=True, quote=False)
     except FloodWait as e:
@@ -35,5 +35,5 @@ async def MediaForward(bot: Client, user_id: int, file_id: int):
 
 async def SendMediaAndReply(bot: Client, user_id: int, file_id: int):
     sent_message = await MediaForward(bot, user_id, file_id)
-    await ReplyForward(message=sent_message, file_id=file_id)
+    #await ReplyForward(message=sent_message, file_id=file_id)
     await asyncio.sleep(2)
